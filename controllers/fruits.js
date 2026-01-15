88e3e87a2b502c6e550088a8314a7c7271928479
Thomas Phillips <fatesunited@gmail.com>
2026-01-14 22:37:29 -0600

    finished error handling lecture

---
+++ controllers/fruits.js
@@ -0,0 +1,27 @@
+// controllers/fruits.js
+const express = require('express');
+const router = express.Router();
+
+const Fruit = require('../models/fruit.js');
+
+router.get('/new', (req, res) => {
+  res.render('fruits/new.ejs');
+});
+
+router.post("/", async (req, res) => {
+  try {
+    await Fruit.create(req.body);
+    req.session.message = "Fruit successfully created.";
+    res.redirect("/fruits");
+  } catch (err) {
+    req.session.message = err.message;
+    res.redirect("/fruits");
+  }
+});
+
+router.get('/', async (req, res) => {
+  const foundFruits = await Fruit.find();
+  res.render('fruits/index.ejs', { fruits: foundFruits });
+});
+
+module.exports = router;
